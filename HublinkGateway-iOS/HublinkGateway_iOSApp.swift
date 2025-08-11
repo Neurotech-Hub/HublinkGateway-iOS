3f0169c9a1cea66abdde24d98aaaf564ca410f68
Matt Gaidica, Ph.D <matt@gaidi.ca>
2025-08-11 09:18:43 -0500

    Initial Commit

---
+++ HublinkGateway-iOS/HublinkGateway_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HublinkGateway_iOSApp.swift
+//  HublinkGateway-iOS
+//
+//  Created by Matt Gaidica on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HublinkGateway_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HublinkGateway-iOSTests/HublinkGateway_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  HublinkGateway_iOSTests.swift
+//  HublinkGateway-iOSTests
+//
+//  Created by Matt Gaidica on 8/11/25.
+//
+
+import Testing
+@testable import HublinkGateway_iOS
+
+struct HublinkGateway_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
